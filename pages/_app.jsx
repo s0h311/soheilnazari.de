bf163e014af972c5d3ea37c6dec4f3c2f6d3efaa
Soheil Nazari <kontakt@soheilnazari.de>
2023-07-16 13:15:34 +0200

    cleanup

---
+++ pages/_app.jsx
@@ -0,0 +1,20 @@
+import '../styles/global.css'
+import Layout from '../components/layout'
+import { Kanit } from 'next/font/google'
+
+const kanit = Kanit({
+  weight: '300',
+  style: 'normal',
+  subsets: ['latin'],
+  variable: '--font-kanit',
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={`${kanit.variable} font-kanit`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  )
+}
