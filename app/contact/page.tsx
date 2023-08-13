eadf47b95e4400613930899bfd335365bc84ba78
Soheil Nazari <kontakt@soheilnazari.de>
2023-08-13 22:16:08 +0200

    updated contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import Content from '@/content/contact.mdx'
 export default function Contact() {
   return (
     <>
-      <section className='place-self-start'>
+      <section className='w-full place-self-start'>
         <Content />
       </section>
       <Cal />
