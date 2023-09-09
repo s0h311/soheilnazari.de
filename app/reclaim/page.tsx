45af72144d8bbb0c7617cc24d4f731dc877c03d5
Soheil Nazari <kontakt@soheilnazari.de>
2023-09-09 12:50:31 +0200

    added experimental reclaim page

---
+++ app/reclaim/page.tsx
@@ -0,0 +1,8 @@
+export default function Reclaim() {
+  return (
+    <iframe
+      className='absolute -bottom-60 w-3/4 h-full'
+      src='https://app.reclaim.ai/e/49b734f7-5013-464f-a805-5a680e59896e'
+    />
+  )
+}
