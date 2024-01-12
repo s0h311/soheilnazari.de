641f4b744bd7703f3033a48af66f0c118a0ed41b
Soheil Nazari <soheil.nazari@check24.de>
2024-01-12 17:09:31 +0100

    refactor: use Reclaim instead of Cal.com, update all packages to latest

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,16 @@
-import Cal from '@/components/cal'
+import Reclaim from '@/components/reclaim'
 import Content from '@/content/contact.mdx'
 
 export default function Contact() {
   return (
-    <>
+    <section className='relative w-full'>
       <section className='w-full place-self-start'>
         <Content />
       </section>
-      <Cal />
-    </>
+      {
+        // <Cal />
+      }
+      <Reclaim />
+    </section>
   )
 }
--- components/reclaim.tsx
+++ components/reclaim.tsx
@@ -1,7 +1,7 @@
 export default function Reclaim() {
   return (
     <iframe
-      className='absolute -bottom-60 w-3/4 h-full'
+      className='rounded-md outline-none w-full h-[100dvh] lg:h-[60dvh]'
       src='https://app.reclaim.ai/e/49b734f7-5013-464f-a805-5a680e59896e'
     />
   )
